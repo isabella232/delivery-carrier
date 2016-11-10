10cf4921b5edf241074a768960abd091cec7198a
vrenaville <vincent.renaville@camptocamp.com>
2016-11-10 14:26:35 +0100

    [FIX] new date for res.partner

--- odoo/songs/updates/update_9_4_1/data.py
+++ odoo/songs/updates/update_9_4_1/data.py
@@ -6,8 +6,13 @@ from pkg_resources import resource_stream, Requirement
 
 import anthem
 from anthem.lyrics.loaders import load_csv_stream
-from anthem.lyrics.records import create_or_update
-import csv
+
+
+@anthem.log
+def import_product_class(ctx, req):
+    """ Importing product template """
+    content = resource_stream(req, 'data/update/product.class.csv')
+    load_csv_stream(ctx, 'product.class', content, delimiter=',')
 
 
 @anthem.log
@@ -16,33 +21,51 @@ def import_product(ctx, req):
     content = resource_stream(req, 'data/update/product.product.csv')
     load_csv_stream(ctx, 'product.product', content, delimiter=',')
 
+
 @anthem.log
 def import_mrp_bom(ctx, req):
-    """ Importing product template """
+    """ Importing bom """
     content = resource_stream(req, 'data/update/mrp.bom.csv')
     load_csv_stream(ctx, 'mrp.bom', content, delimiter=',')
 
 
 @anthem.log
 def import_pricelist_item(ctx, req):
-    """ Import Company and Individual  """
-    csv_content = resource_stream(req, 'data/update/product.pricelist.item.csv')
+    """ Import  pricelist  """
+    csv_content = resource_stream(
+        req, 'data/update/product.pricelist.item.csv')
     load_csv_stream(ctx, 'product.pricelist.item', csv_content, delimiter=',')
 
 
 @anthem.log
-def load_res_partner_contact(ctx, req):
-    """ Import Contacts in companies  """
-    csv_content = resource_stream(req, 'data/update/res.partner.contact.csv')
-    load_csv_stream(ctx, 'res.partner', csv_content, delimiter=',')
+def import_partner_part(ctx, req, file_part):
+    """ Import partner  """
+    with ctx.log(u'Import Partner %s' % str(file_part)):
+        csv_content = resource_stream(
+            req, 'data/update/res.partner.%s.csv' % str(file_part).zfill(3))
+        load_csv_stream(ctx, 'res.partner', csv_content, delimiter=',')
+
+
+@anthem.log
+def import_partner_contact_part(ctx, req, file_part):
+    """ Import partner  """
+    with ctx.log(u'Import Partner Contact %s' % str(file_part)):
+        csv_content = resource_stream(
+            req,
+            'data/update/res.partner_[contacts_only]).%s.csv' % str(
+                file_part).zfill(3))
+        load_csv_stream(ctx, 'res.partner', csv_content, delimiter=',')
 
 
 @anthem.log
 def main(ctx):
     """ Loading data """
-    req = Requirement.parse('qoqa-odoo')
-    import_product(ctx, req)
-    import_mrp_bom(ctx, req)
-    import_pricelist_item(ctx, req)
-    # load_res_partner(ctx)
-    # load_res_partner_contact(ctx)
+    req = Requirement.parse('swisslux-odoo')
+    # import_product_class(ctx, req)
+    # import_product(ctx, req)
+    # import_mrp_bom(ctx, req)
+    # import_pricelist_item(ctx, req)
+    for seq_file in xrange(1, 13):
+        import_partner_part(ctx, req, seq_file)
+    for seq_file in xrange(1, 20):
+        import_partner_contact_part(ctx, req, seq_file)
