72b9e5499bafd59fdb17a4764eddf92c1d62e66c
Joël Grand-Guillaume <joel.grandguillaume@gmail.com>
2017-02-08 15:00:46 +0100

    [ADD] module specific_purchase to propagate proc. group

---
+++ odoo/local-src/specific_purchase/README.rst
@@ -0,0 +1,5 @@
+Specific changes for purchase
+-----------------------------
+
+* Propagate the procurement group to chained moves when PO is created from a Order point if the rules used has check "propagate cancel and split". Ensure
+that all moves belong to a separate picking for each PO.
---
+++ odoo/local-src/specific_purchase/models/stock_move.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Joel Grand-Guillaume (Camptocamp)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _propagate_procurement_group(self, group_id):
+        """ Propagate the proc. group to all chain moves if propagate is True.
+        Goal is to have all moves related to a same PO in a same picking all 
+        along the chain event if the PO has been generated from a OP.
+        @return: True
+        """
+        res = self.write({'group_id': group_id.id, 'picking_id': False})
+        self._picking_assign()
+        for move in self:
+            if move.move_dest_id and move.propagate:
+                move.move_dest_id._propagate_procurement_group(move.group_id)
+        return res
