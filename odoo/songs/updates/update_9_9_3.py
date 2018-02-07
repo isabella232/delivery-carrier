7ed5e4c082d054b649239f098e5ee735c5ddd277
Denis Leemann <denis.leemann@camptocamp.com>
2018-02-07 11:11:48 +0100

    Move 9.9.2 to 9.9.3

--- odoo/songs/updates/update_9_9_3.py
+++ odoo/songs/updates/update_9_9_3.py
@@ -6,25 +6,10 @@
 import anthem
 
 
-@anthem.log
-def remove_translation(ctx, module_name):
-    """ remove translation """
-    ctx.env['ir.translation'].search(
-        [('module', '=', module_name)]
-    ).unlink()
 
-
-@anthem.log
-def reload_translation(ctx, module_name):
-    """ update translation """
-    ctx.env['ir.module.module'].with_context(overwrite=True).search(
-        [('name', '=', module_name)]
-    ).update_translations()
 
 
 @anthem.log
 def main(ctx):
-    """ Loading data """
-    module_name = 'specific_reports'
-    remove_translation(ctx, module_name)
-    reload_translation(ctx, module_name)
+
+
