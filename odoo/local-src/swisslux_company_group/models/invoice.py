b43ded8a6d421160e8f553079291fc83a91f358b
jcoux <julien.coux@camptocamp.com>
2018-06-01 15:07:07 +0200

    BSSLX-82: Change behavior of field company_group on invoices

--- odoo/local-src/swisslux_company_group/models/invoice.py
+++ odoo/local-src/swisslux_company_group/models/invoice.py
@@ -1,18 +1,20 @@
 # Copyright 2015 Swisslux
 # Copyright 2016 Yannick Vaucher (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     company_group_id = fields.Many2one(
-        related='partner_id.company_group_id',
-        store=True,
-        readonly=True,
-        model='res.partner',
+        comodel_name='res.partner',
         string='Company Group',
+    )
+
+    company_group_readonly_id = fields.Many2one(
+        related='company_group_id',
+        readonly=True,
     )
 
     income_partner_id = fields.Many2one(
@@ -20,3 +22,10 @@ class AccountInvoice(models.Model):
         string='Income Partner',
         domain=([('is_company', '=', True)])
     )
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        if self.partner_id:
+            self.company_group_id = self.partner_id.company_group_id.id
+        else:
+            self.company_group_id = None
