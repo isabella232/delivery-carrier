a49f5058e4b20ce4d3b68f99f8dcd57f4e0b7fd8
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-06-22 16:21:51 +0300

    [ADD] Add swisslux_lang module BSSLX-96
    Basically, it is just an easy way to reset language configuration to
    desired state, defined in a sole XML file inside the module.

---
+++ odoo/local-src/swisslux_lang/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2016-2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Swisslux - Language configuration',
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'Swisslux Modules',
+    'website': 'http://www.swisslux.ch',
+    'images': [],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/res_lang.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
