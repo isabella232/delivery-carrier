c33a7a63a56206ad336062af3a8934f9f1955122
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-10-21 09:19:14 +0200

    tmp

--- delivery_free_fee_removal/models/sale_order_line.py
+++ delivery_free_fee_removal/models/sale_order_line.py
@@ -1,16 +1,25 @@
 # Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.depends("is_delivery", "price_unit")
+    is_free_delivery = fields.Boolean(
+        compute="_compute_is_free_delivery",
+        store=True,
+    )
+
+    @api.depends("is_delivery", "currency_id", "price_unit")
+    def _compute_is_free_delivery(self):
+        for line in self:
+            line.is_free_delivery = line.is_delivery and line.currency_id.is_zero(line.price_unit)
+
+    @api.depends("is_free_delivery")
     def _get_to_invoice_qty(self):
-        free_delivery_lines = self.filtered(
-            lambda line: line.is_delivery and line.currency_id.is_zero(line.price_unit))
+        free_delivery_lines = self.filtered("is_free_delivery")
         free_delivery_lines.qty_to_invoice = 0
         other_lines = self - free_delivery_lines
         super(SaleOrderLine, other_lines)._get_to_invoice_qty()
