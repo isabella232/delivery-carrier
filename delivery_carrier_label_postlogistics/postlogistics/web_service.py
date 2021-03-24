8f3bcfe053719ea25c7a70ea1c14c6d828298443
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2021-03-24 16:28:49 +0100

    [12.0] delivery_carrier_label_postlogistics: Fix encoding to handle accented chars

--- delivery_carrier_label_postlogistics/postlogistics/web_service.py
+++ delivery_carrier_label_postlogistics/postlogistics/web_service.py
@@ -20,13 +20,6 @@ _compile_itemid = re.compile(r'[^0-9A-Za-z+\-_]')
 _compile_itemnum = re.compile(r'[^0-9]')
 
 
-def _sanitize_string(string):
-    """Temporary fix, remove all non ascii char as the API
-    weirdly doesn't support them in some cases.
-    """
-    return unidecode.unidecode(string)
-
-
 class PostlogisticsWebService(object):
 
     """ Connector with PostLogistics for labels using post.ch API
@@ -75,8 +68,8 @@ class PostlogisticsWebService(object):
 
         partner_name = partner.name or partner.parent_id.name
         recipient = {
-            'name1': _sanitize_string(partner_name),
-            'street': _sanitize_string(partner.street),
+            'name1': partner_name,
+            'street': partner.street,
             'zip': partner.zip,
             'city': partner.city,
             'email': partner.email or None,
@@ -86,10 +79,10 @@ class PostlogisticsWebService(object):
             recipient['country'] = partner.country_id.code.upper()
 
         if partner.street2:
-            recipient['addressSuffix'] = _sanitize_string(partner.street2)
+            recipient['addressSuffix'] = partner.street2
 
         if partner.parent_id and partner.parent_id.name != partner_name:
-            recipient['name2'] = _sanitize_string(partner.parent_id.name)
+            recipient['name2'] = partner.parent_id.name
             recipient['personallyAddressed'] = False
 
         # Phone and / or mobile should only be displayed if instruction to
@@ -120,8 +113,8 @@ class PostlogisticsWebService(object):
         partner = company.partner_id
 
         customer = {
-            'name1': _sanitize_string(partner.name),
-            'street': _sanitize_string(partner.street),
+            'name1': partner.name,
+            'street': partner.street,
             'zip': partner.zip,
             'city': partner.city,
             'country': partner.country_id.code,
@@ -445,7 +438,7 @@ class PostlogisticsWebService(object):
                     'accept': 'application/json',
                     'content-type': 'application/json',
                 },
-                data=json.dumps(data),
+                data=json.dumps(data, ensure_ascii=False).encode("utf-8"),
                 timeout=60,
             )
 
