b68b78ea29c8c94b9dace45752bd26e20415a008
Telmo Santos <telmo.santos@camptocamp.com>
2021-03-29 16:13:41 +0200

    Add pack_weight parameter on _prepare_attributes

--- delivery_postlogistics_dangerous_goods/postlogistics/web_service.py
+++ delivery_postlogistics_dangerous_goods/postlogistics/web_service.py
@@ -10,7 +10,8 @@ from odoo.addons.delivery_postlogistics.postlogistics import web_service
 UNNUMBER_REGEX = re.compile("^[0-9]{1,4}$")
 
 
-class PostlogisticsWebServiceDangerousGoods(web_service.PostlogisticsWebService):
+class PostlogisticsWebServiceDangerousGoods(
+    web_service.PostlogisticsWebService):
     def _get_unnumbers(self, picking, pack=None):
         """
         If any dangerous goods with limited quantity, returns a list of UNNumbers.
@@ -20,9 +21,11 @@ class PostlogisticsWebServiceDangerousGoods(web_service.PostlogisticsWebService)
             and pack.mapped("quant_ids.product_id")
             or picking.mapped("move_lines.product_id")
         )
-        limited_amount_lq = picking.env.ref("l10n_eu_product_adr.limited_amount_1")
+        limited_amount_lq = picking.env.ref(
+            "l10n_eu_product_adr.limited_amount_1")
         limited_quantity_products = products.filtered(
-            lambda p: p.is_dangerous and p.limited_amount_id == limited_amount_lq
+            lambda
+                p: p.is_dangerous and p.limited_amount_id == limited_amount_lq
         )
         if limited_quantity_products:
             unnumbers = []
@@ -37,10 +40,12 @@ class PostlogisticsWebServiceDangerousGoods(web_service.PostlogisticsWebService)
                 unnumbers.append(int(unnumber))
             return unnumbers
 
-    def _prepare_attributes(self, picking, pack=None, pack_num=None, pack_total=None):
+    def _prepare_attributes(self, picking, pack=None, pack_num=None,
+                            pack_total=None, pack_weight=None):
         # Adds a new attribute UNNumbers when there's dangerous goods
         # in the pack / picking
-        res = super()._prepare_attributes(picking, pack, pack_num, pack_total)
+        res = super()._prepare_attributes(picking, pack, pack_num, pack_total,
+                                          pack_weight)
         unnumbers = self._get_unnumbers(picking, pack)
         if unnumbers:
             res["unnumbers"] = unnumbers
